838bba5136ef35379bb3609b043e4f87a2b3147c
Neil Empey <argus.two.2@gmail.com>
2020-04-13 19:22:15 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,10 @@
+def introduction(name, language)
+  puts "Hi, my name is #{name}.\n"
+end
+
+def introduction_with_language(name, language)
+ puts "Hi, my name is #{name} and I'm learning to program in #{language}.\n"
+ end 
+ 
+puts introduction('neil')
+puts introduction_with_language('neil', 'Ruby')
